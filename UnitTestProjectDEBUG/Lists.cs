e2344a741640d578c8b211e309715ac22e62a22f
Wendrowski <rasstrigin1349@gmail.com>
2022-09-21 18:39:41 +0200

    ArrayList Practice

--- UnitTestProjectDEBUG/Lists.cs
+++ UnitTestProjectDEBUG/Lists.cs
@@ -45,16 +45,36 @@ namespace UnitTestProjectDEBUG
             }
             return sum;
         }
-
-
-
-
-        /*public int TotalScores()
+        [TestMethod]
+        public void ArrayList_Practice()
         {
+            ArrayList list = new ArrayList();
+            Console.WriteLine("Adding some numbers: ");
+            list.Add(45);
+            list.Add(78);
+            list.Add(33);
+            list.Add(56);
+            list.Add(12);
+            list.Add(23);
+            list.Add(9);
 
-        }*/
+            Console.WriteLine("Capacity: {0}", list.Capacity);
+            Console.WriteLine("Count: {0} ", list.Count);
 
+            Console.WriteLine("Content: ");
+            foreach (int i in list)
+            {
+                Console.WriteLine(i + " ");
+                Console.WriteLine();
+            }
 
-
+            Console.WriteLine("Sorted Content: ");
+            list.Sort();
+            foreach (int i in list)
+            {
+                Console.WriteLine(i + " ");
+            }
+            Console.WriteLine();            
+        }
     }
 }
